657231eeecb5552c5cf69302593a24df6c21eb15
pdo9141 <pdo9141@gmail.com>
2017-10-31 14:23:51 -0700

    Initial psadmin

---
+++ ReactDemo/psadmin/src/components/homePage.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function () {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+                <p>React, React Router, and Flux for ultra-responseive web apps.</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
